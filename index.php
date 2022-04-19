9b4fd331fece72addbc8ad63c3adb76ed1a70e93
Dorine G <lapattedugoupil@gmail.com>
2022-04-19 11:37:22 +0200

    Ajout du menu et de la page d'accueil

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wikifruit</title>
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer">
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+    <?php include 'core/menu.php' ?>
+
+    <div class="container-fluid">
+    <div class="row">
+        <div class="col-12 col-md-8 mx-auto py-5">
+
+            <h1 class="pb-4">Acceuil Wikifruit</h1>
+
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum eligendi ab, ipsa inventore nesciunt dolorem veniam ea quia perspiciatis sunt in mollitia, dicta voluptatum voluptas rerum ipsum deserunt magni ullam ratione consectetur repudiandae aspernatur. Beatae iure dicta repellat cupiditate unde, veniam accusantium asperiores rerum reiciendis vel temporibus iste! Rem, fugiat?</p>
+        </div>
+    </div>
+</div>
+
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <script src="js/script.js"></script>
+</body>
+</html>
